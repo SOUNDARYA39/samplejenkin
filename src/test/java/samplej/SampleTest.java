801db30ecdf57345d2b9c45c8a48a7ae1b673da1
SOUNDARYA39 <soundaryagpt.g2@gmail.com>
2023-07-12 11:34:58 +0530

    firstcommit

---
+++ src/test/java/samplej/SampleTest.java
@@ -0,0 +1,20 @@
+package samplej;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void m1()
+	{
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void m2() 
+	{
+		System.out.println("hi");
+	}
+
+
+}
